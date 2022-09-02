0e3b6a12b07487b6334fe39c9b7305548c77e3b9
Adriano Cruz <adrianorc@outlook.com>
2022-09-02 14:05:16 -0400

    Static pages

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 # from .models import related models
-
+from .models import CarMake, CarModel
 
 # Register your models here.
-
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2
 # CarModelInline class
+class CarModelAdmin(admin.ModelAdmin):
+
+    
 
 # CarModelAdmin class
-
+    list_display = ["make",'name',"id", 'type', 'year']
+    list_filter = ['year']
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'description')
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
