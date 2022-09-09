370d1aa07f9a40975226577df292ac8625fc9d0a
brunaccruz137 <brunaccruz137@gmail.com>
2022-09-09 14:47:59 +0000

    Functions

---
+++ functions/get_dealerships.js
@@ -0,0 +1,42 @@
+const Cloudant = require('@cloudant/cloudant');
+  
+const credentials = {
+    COUCH_URL: '',
+    IAM_API_KEY: ''
+}
+
+function main(params) {
+  
+  const cloudant = Cloudant({
+      url: credentials.COUCH_URL,
+      plugins: { iamauth: { iamApiKey: credentials.IAM_API_KEY } }
+  });
+  
+  const Filter = params.state !== undefined ? {
+      selector: { st: { "$eq": params.state.replace(/"/g, '') }, },
+      fields: [ "id", "city", "state", "st", "address", "zip", "lat", "long", "short_name", "full_name" ]
+  } : params.dealerId !== undefined ? {
+      selector: { id: { "$eq": parseInt(params.dealerId) } },
+      fields: [ "id", "city", "state", "st", "address", "zip", "lat", "long", "short_name", "full_name" ]
+  } : {
+      selector: { },
+      fields: [ "id", "city", "state", "st", "address", "zip", "lat", "long", "short_name", "full_name" ]
+  }
+  
+  
+  let dbListPromise = get_dealerships(cloudant, Filter);
+  
+  return dbListPromise
+}
+
+function get_dealerships(cloudant, findO) {
+  return new Promise((resolve, reject) => {
+      cloudant.use('dealerships').find(findO)
+          .then(resp => {
+              resolve({ rows: resp.docs });
+          })
+          .catch(err => {
+              reject({ err: err });
+          });
+  });   
+}
--- functions/sample/nodejs/index-promise.js
+++ functions/sample/nodejs/index-promise.js
@@ -2,16 +2,17 @@
  * Get all dealerships
  */
 
-const { CloudantV1 } = require('@ibm-cloud/cloudant');
-const { IamAuthenticator } = require('ibm-cloud-sdk-core');
+const { Cloudant } = require('@cloudant/cloudant');
+//const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
 function main(params) {
 
-    const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
-    const cloudant = CloudantV1.newInstance({
-      authenticator: authenticator
+    //const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
+    const cloudant = Cloudant({
+        url: params.COUCH_URL,
+        plugins: { iamauth: { iamApiKey: params.IAM_API_KEY }}
     });
-    cloudant.setServiceUrl(params.COUCH_URL);
+
 
     let dbListPromise = getDbs(cloudant);
     return dbListPromise;
@@ -19,12 +20,11 @@ function main(params) {
 
 function getDbs(cloudant) {
      return new Promise((resolve, reject) => {
-         cloudant.getAllDbs()
+         cloudant.db.list()
              .then(body => {
                  resolve({ dbs: body.result });
              })
              .catch(err => {
-                  console.log(err);
                  reject({ err: err });
              });
      });
@@ -33,25 +33,23 @@ function getDbs(cloudant) {
  
  /*
  Sample implementation to get the records in a db based on a selector. If selector is empty, it returns all records. 
- eg: selector = {state:"Texas"} - Will return all records which has value 'Texas' in the column 'State'
- */
- function getMatchingRecords(cloudant,dbname, selector) {
+
+ function getDbs(cloudant) {
      return new Promise((resolve, reject) => {
-         cloudant.postFind({db:dbname,selector:selector})
-                 .then((result)=>{
-                   resolve({result:result.result.docs});
-                 })
-                 .catch(err => {
-                    console.log(err);
-                     reject({ err: err });
+        cloudant.db.list()
+        .then(body => {
+            resolve({ dbs: body });
+        })
+        .catch(err => {
+            reject({ err: err });
                  });
           })
  }
  
                         
- /*
+
  Sample implementation to get all the records in a db.
- */
+
  function getAllRecords(cloudant,dbname) {
      return new Promise((resolve, reject) => {
          cloudant.postAllDocs({ db: dbname, includeDocs: true, limit: 10 })            
@@ -64,3 +62,4 @@ function getDbs(cloudant) {
              });
          })
  }
+ */
